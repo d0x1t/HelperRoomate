b5f63b4754c553098261ac61b788a856279e1db7
d0x1t <lapariello86@gmail.com>
2024-07-02 21:38:36 +0200

    Aggiornamento dei file

--- HelperRoomate/ContentView.swift
+++ HelperRoomate/ContentView.swift
@@ -1,24 +1,60 @@
 //
 //  ContentView.swift
-//  HelperRoomate
+//  Scan-Ocr
 //
-//  Created by Studente on 02/07/24.
+//  Created by Haaris Iqubal on 5/21/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showScannerSheet = false
+    @State private var texts:[ScanData] = []
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            VStack{
+                if texts.count > 0{
+                    List{
+                        ForEach(texts){text in
+                            NavigationLink(
+                                destination:ScrollView{Text(text.content)},
+                                label: {
+                                    Text(text.content).lineLimit(1)
+                                })
+                        }
+                    }
+                }
+                else{
+                    Text("No scan yet").font(.title)
+                }
+            }
+                .navigationTitle("Scan OCR")
+                .navigationBarItems(trailing: Button(action: {
+                    self.showScannerSheet = true
+                }, label: {
+                    Image(systemName: "doc.text.viewfinder")
+                        .font(.title)
+                })
+                .sheet(isPresented: $showScannerSheet, content: {
+                    self.makeScannerView()
+                })
+                )
         }
-        .padding()
+    }
+    private func makeScannerView()-> ScannerView {
+        ScannerView(completion: {
+            textPerPage in
+            if let outputText = textPerPage?.joined(separator: "\n").trimmingCharacters(in: .whitespacesAndNewlines){
+                let newScanData = ScanData(content: outputText)
+                self.texts.append(newScanData)
+            }
+            self.showScannerSheet = false
+        })
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
--- HelperRoomate/ScanData.swift
+++ HelperRoomate/ScanData.swift
@@ -1,8 +1,18 @@
 //
 //  ScanData.swift
-//  HelperRoomate
+//  Scan-Ocr
 //
-//  Created by Studente on 02/07/24.
+//  Created by Haaris Iqubal on 5/21/21.
 //
 
 import Foundation
+
+
+struct ScanData:Identifiable {
+    var id = UUID()
+    let content:String
+    
+    init(content:String) {
+        self.content = content
+    }
+}

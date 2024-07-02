eafc7fd88a4200d3b056b7ddab22d8f1ce0f5412
d0x1t <lapariello86@gmail.com>
2024-07-02 21:06:23 +0200

    Initial Commit

---
+++ HelperRoomate/HelperRoomateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelperRoomateApp.swift
+//  HelperRoomate
+//
+//  Created by Studente on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HelperRoomateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
